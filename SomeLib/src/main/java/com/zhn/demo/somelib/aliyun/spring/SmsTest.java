cf449abea732d2cc27df7e9f66824adb4808d887
makingTopper <961848179@qq.com>
2021-05-11 14:17:45 +0800

    删除测试代码中个人隐私

--- SomeLib/src/main/java/com/zhn/demo/somelib/aliyun/spring/SmsTest.java
+++ SomeLib/src/main/java/com/zhn/demo/somelib/aliyun/spring/SmsTest.java
@@ -14,15 +14,14 @@ public class SmsTest {
     @Test
     public void testSms() {
         Sms sms = new Sms();
-        System.out.println(sms.sendSmsOnIDVerity("4875", "18988590166"));
-//        System.out.println(sms.sendSmsOnInfo("4875", "18774220168"));
-//        System.out.println(sms.sendSmsOnLogin("4875", "18774220168"));
-//        System.out.println(sms.sendSmsOnLoginErr("4875", "18774220168"));
-//        System.out.println(sms.sendSmsOnRegister("4875", "18774220168"));
-//        System.out.println(sms.sendSmsOnPassword("4875", "18774220168"));
+        System.out.println(sms.sendSmsOnIDVerity("4875", "18888888888"));
+//        System.out.println(sms.sendSmsOnInfo("4875", "18888888888"));
+//        System.out.println(sms.sendSmsOnLogin("4875", "18888888888"));
+//        System.out.println(sms.sendSmsOnLoginErr("4875", "18888888888"));
+//        System.out.println(sms.sendSmsOnRegister("4875", "18888888888"));
+//        System.out.println(sms.sendSmsOnPassword("4875", "18888888888"));
 //        List<String> list = new ArrayList<>();
-//        list.add("18774220168");
-//        list.add("13927400695");
+//        list.add("18888888888");
 //        System.out.println(sms.sendSmsOnWarn(map, list));
         String uri = "https://code.com";
 
@@ -37,7 +36,7 @@ public class SmsTest {
 
         System.out.println(JSONObject.toJSONString(map));
         Vms vms = new Vms();
-        System.out.println(vms.sendTextCallOnMan("18774220168", map));
+        System.out.println(vms.sendTextCallOnMan("18888888888", map));
     }
 
 }
